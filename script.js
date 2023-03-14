9b81a6af7bcd8b8bfbb291762000656b1b2e2709
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-14 11:50:53 -0300

    criando primeiro projeto js

---
+++ script.js
@@ -0,0 +1,22 @@
+alert("vem deslizando vai")
+
+    var nome="livs"
+    console.log(nome)
+
+    var idade=18
+    console.log(idade)
+
+    var sobrenome="namba"
+    console.log(sobrenome)
+
+    var sexo="feminino"
+    console.log(sexo)
+
+    var time="corinthians"
+    console.log(time)
+
+    var comida="pizza"
+    console.log(comida)
+
+    var animal_de_estimação="furão"
+    console.log(animal_de_estimação)
